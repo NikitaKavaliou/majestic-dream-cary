b22c87c4ca4d32b564b329b9d60e948e34c5e41b
Accu 140 <Accu140@accu.local>
2026-02-04 08:04:42 -0500

    change include

--- js/include.js
+++ js/include.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("components/header.html")
+  fetch("../components/header.html")
     .then(res => res.text())
     .then(data => document.querySelector("header").innerHTML = data);
 
-  fetch("components/footer.html")
+  fetch("../components/footer.html")
     .then(res => res.text())
     .then(data => document.querySelector("footer").innerHTML = data);
 });
